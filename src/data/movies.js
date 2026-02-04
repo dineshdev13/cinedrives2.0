cc629159ab415c4dbec80c6b71cd8a7b1a4f041f
DINESHKUMAR P <142890939+dineshdev13@users.noreply.github.com>
2026-02-04 17:12:23 +0530

    Update movies.js

--- src/data/movies.js
+++ src/data/movies.js
@@ -5,8 +5,9 @@ export const movies = [
         year: 2025,
         genre: "Horror, Comedy, Fantasy",
         description: "A light-hearted supernatural story where a man’s life changes after an unexpected encounter with a ghost, blending humor, emotion, and mystery.",
+        cast: ["Nivin Pauly, Riya Shibu, Aju Varghese, Janardhanan, Preity Mukhundhan"]
         posterUrl: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/sarvam-maya-et00473343-1766390933.jpg",
         downloadLink: "https://drive.google.com/file/d/1ZhbCdk727qXjw350nULs1XA_ORCRRg5_/view?usp=drive_link" // Replace with Drive Link
-        cast: ["Nivin Pauly", "Riya Shibu", "Aju Varghese", "Janardhanan", "Preity Mukhundhan"]
+        
     }
 ];
